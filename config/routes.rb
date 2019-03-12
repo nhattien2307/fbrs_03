8b58107f67d6a4f7b977e9c8cd07edc30bce6212
nhattien <nhattien2307@gmail.com>
2019-03-12 14:11:16 +0700

    log_in

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "users/new"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
