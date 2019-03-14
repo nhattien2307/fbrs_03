8cd9ac7b73db5ff5c46e7850a0848e5b7a09ecc3
nhattien <nhattien2307@gmail.com>
2019-03-14 11:36:25 +0700

    review, rate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
 
-  resources :books
+  resources :books do
+    resources :reviews
+  end
   resources :users
 
   get "users/new"
---
+++ test/controllers/reviews_controller_test.rb
@@ -0,0 +1,3 @@
+require "test_helper"
+class ReviewsControllerTest < ActionDispatch::IntegrationTest
+end
